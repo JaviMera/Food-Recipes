c717c33573d19f5bc492118303e45849d5e2249d
javier <javier_mera88@hotmail.com>
2022-08-14 14:45:21 -0500

    Added navigation component with three basic destinations.

--- app/src/main/java/com/example/foodrecipes/MainActivity.kt
+++ app/src/main/java/com/example/foodrecipes/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.foodrecipes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var _navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        _navController = (supportFragmentManager
+            .findFragmentById(R.id.navigationHostFragment) as NavHostFragment
+                ).navController
+
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.recipesFragment,
+            R.id.favoriteRecipesFragment,
+            R.id.foodJokeFragment
+        ))
+
+        bottomNavigationView.setupWithNavController(_navController)
+        setupActionBarWithNavController(_navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return _navController.navigateUp() ||  super.onSupportNavigateUp()
     }
 }
