b3e35663dcc894bd070e8609b1bf6e49083e13da
javier <javier_mera88@hotmail.com>
2022-08-15 08:19:30 -0500

    Added recipes recycler view and shimmer frame layout.

--- app/src/main/java/com/example/foodrecipes/RecipesFragment.kt
+++ app/src/main/java/com/example/foodrecipes/RecipesFragment.kt
@@ -1,10 +1,12 @@
 package com.example.foodrecipes
 
+import android.opengl.Visibility
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
 class RecipesFragment : Fragment() {
 
@@ -13,6 +15,9 @@ class RecipesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
+        view.recipes_recycler_view.visibility = View.GONE
+        view.shimmer_layout.startShimmer()
+        return view
     }
 }
