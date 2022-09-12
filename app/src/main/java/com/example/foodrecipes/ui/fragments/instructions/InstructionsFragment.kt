2aa30180ad8cca863675a3a08ba796b66cb14f0e
javier <javier_mera88@hotmail.com>
2022-09-12 08:16:09 -0500

    Added webview to display instructions in instructions fragment.

--- app/src/main/java/com/example/foodrecipes/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/example/foodrecipes/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,28 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
+import androidx.lifecycle.ViewModelProvider
 import com.example.foodrecipes.R
 import com.example.foodrecipes.databinding.FragmentInstructionsBinding
 import com.example.foodrecipes.util.Constants
+import com.example.foodrecipes.viewmodels.MainViewModel
 
 class InstructionsFragment : Fragment() {
 
     private var _binding: FragmentInstructionsBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var mainViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mainViewModel = ViewModelProvider(requireActivity())[MainViewModel::class.java]
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         _binding = FragmentInstructionsBinding.inflate(layoutInflater, container, false)
 
-        val resultId = arguments?.getInt(Constants.RECIPE_RESULT_KEY)
+        val resultId = arguments?.getInt(Constants.RECIPE_RESULT_KEY)!!
+        binding.instructionsWebview.webViewClient = object : WebViewClient() {}
 
+        mainViewModel.getRecipe(resultId)
+        mainViewModel.recipeResponse.observe(viewLifecycleOwner){
+            it.data?.let { recipe ->
+                binding.instructionsWebview.loadUrl(recipe.sourceUrl)
+            }
+        }
         return binding.root
     }
 }
